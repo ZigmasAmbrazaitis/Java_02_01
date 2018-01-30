267f6336c0c77de5ff1e6fd0d4581ef5f432c77e
Zigmas <zambrazaitis@gmail.com>
2018-01-30 14:32:05 +0200

    Type name 5 times.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite savo varda: ");
+        String vardas = sc.nextLine();
+        System.out.println("Jusu vardas: " + vardas);
     }
 }
